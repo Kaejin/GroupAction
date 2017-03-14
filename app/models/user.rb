63a9b7c70d64e7ff63988c80737adba6b822ae96
Archie Gilbert <archiegilbert@me.com>
2017-03-14 12:43:12 +0000

    charitys controller added and appropriate associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :event_donations
+  has_many :direct_donations
+  has_many :events
+  has_many :fundraising_events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
