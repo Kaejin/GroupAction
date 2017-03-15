8300e2e9ac5b209b0c80ca93746aea105dec4589
Archie Gilbert <archiegilbert@me.com>
2017-03-15 11:38:26 +0000

    users can now add a donation to an event

---
+++ app/policies/event_donation_policy.rb
@@ -0,0 +1,19 @@
+class EventDonationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    record.user == user
+  end
+end
