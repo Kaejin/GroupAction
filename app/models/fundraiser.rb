b4ff011f218d73412a2af512008006abdf52a009
Josh Oppenheim <joppenheim1@gmail.com>
2017-03-27 14:11:43 +0100

    moving onto the polymorphic part

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -11,6 +11,7 @@ class Fundraiser < ApplicationRecord
   def total_raised
     self.amount_raised = 0
     self.direct_donations.each { |donation| self.amount_raised += donation.amount }
+    self.offline_donations.each { |donation| self.amount_raised += donation.amount }
     self.events.each { |event| self.amount_raised += event.total_raised }
     self.amount_raised
   end
