abe869111041bd21e36434fa2daf17a8a574662a
Josh Oppenheim <joppenheim1@gmail.com>
2017-03-31 22:49:08 +0100

    done some shiz

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -7,4 +7,14 @@ class Charity < ApplicationRecord
     self.user.fundraisers.each { |fundraiser| self.amount_raised += fundraiser.total_raised unless fundraiser.total_raised.nil? }
     self.amount_raised
   end
+
+  def number_donations
+    no_donations = 0
+    self.user.fundraisers.each do  |fundraiser|
+      no_donations += fundraiser.direct_donations.count
+      fundraiser.events.each { |event| no_donations += event.event_donations.count }
+    end
+    no_donations
+  end
+
 end
